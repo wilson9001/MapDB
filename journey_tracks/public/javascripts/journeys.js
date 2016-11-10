30dcd3fdb3f19eb78196b53e2340f10b0ec234d0
wilson9001 <wilsonalexanderjohn@gmail.com>
2016-11-10 07:00:37 +0000

    Re-did the page like 5 thousand times.

--- journey_tracks/public/javascripts/journeys.js
+++ journey_tracks/public/javascripts/journeys.js
@@ -1,7 +1,42 @@
  var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 0, lng: 0},
-          zoom: 3
+      function initMap() 
+      {
+        map = new google.maps.Map(document.getElementById('map'), 
+        {
+          zoom: 2,
+          center: new google.maps.LatLng(0,0),
+          mapTypeId: 'terrain'
         });
+
+         map.data.loadGeoJson('javascripts/samplepoints.json');
+
+         /*
+        // Create a <script> tag and set the USGS URL as the source.
+        var script = document.createElement('script');
+        // This example uses a local copy of the GeoJSON stored at
+        // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+        script.src = map.data.loadGeoJson('samplepoints.json');
+        //'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+        //map.data.loadGeoJson('data.json');
+        */
+
+        //document.getElementsByTagName('head')[0].appendChild(script);
       }
+
+      // Loop through the results array and place a marker for each
+      // set of coordinates.
+      /*
+      window.eqfeed_callback = function(results) 
+      {
+        for (var i = 0; i < results.points.length; i++) 
+        {
+          var coords = results.points[i].coordinates;
+          var latLng = new google.maps.LatLng(coords[1],coords[0]);
+          var marker = new google.maps.Marker(
+            {
+            position: latLng,
+            map: map
+            });
+        }
+      }
+      */
--- journey_tracks/routes/index.js
+++ journey_tracks/routes/index.js
@@ -1,9 +1,57 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/locationsDB');
+
+var locationSchema = mongoose.Schema(
+    {
+      location : Object
+    })
+
+    var point = mongoose.model('point', locationSchema);
+
+    var db = mongoose.connection;
+
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function()
+    {
+      console.log('Connected');
+    });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile('journeys.html', { root: 'public' });
 });
+
+router.post('/location', function(req, res, next)
+{
+  console.log("POST Location Route");
+
+var newlocation = new location(req.body);
+console.log(newlocation);
+
+newlocation.save(function(err, post)
+{
+  if(err) return console.error(err);
+  console.log(post);
+  res.sendStatus(200);
+});
+
+})
+
+router.get('/location', function(req, res, next)
+{
+  console.log("In the GET route?");
+  point.find(function(err,locationList)
+  {
+    if(err) return console.error(err);
+    else
+    {
+      console.log(locationList);
+      res.json(locationList);
+    }
+  })
+})
 
 module.exports = router;
